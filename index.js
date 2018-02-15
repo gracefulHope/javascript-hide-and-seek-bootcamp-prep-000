69182c1bd371b95c8115bc976c3908cffe46222d
Sara S <ewf.gloria@gmail.com>
2018-02-15 21:36:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function nestedTarget(){
 
 function increaseRankBy(n){
   const lis = document.querySelectorAll('.ranked-list li')
-  console.log(lis)
   for (let i = 0; i < lis.length; i++) {
     lis[i].innerHTML = (parseInt(lis[i]) + n).toString()
   }
